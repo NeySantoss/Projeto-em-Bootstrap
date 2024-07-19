e6f38596a78a21e8604df6e59ca18f0d64d576aa
Ney Santos <neyysss@hotmail.com>
2024-07-19 20:37:03 -0300

    scroll animate

--- js/script.js
+++ js/script.js
@@ -153,6 +153,41 @@ $(document).ready(function() {
         }
     }
 
+    //Scroll section
+    let navBtn = $('.nav-item');
 
+    let bannerSection = $('#mainSlider');
+    let aboutSection = $('#about-area');
+    let servicesSection = $('#service-area');
+    let teamSection = $('#team-area');
+    let portfolioSection = $('#portifolio-area');
+    let contactSection = $('#contact-area');
 
+    let scrollTo = '';
+
+    $(navBtn).click(function() {
+
+        let btnId = $(this).attr('id');
+
+        if(btnId == 'about-menu') {
+        scrollTo = aboutSection;
+        } else if(btnId == 'service-menu') {
+        scrollTo = servicesSection;
+        } else if(btnId == 'team-menu') {
+        scrollTo = teamSection;
+        } else if(btnId == 'portfolio-menu') {
+        scrollTo = portfolioSection;
+        } else if(btnId == 'contact-menu') {
+        scrollTo = contactSection;
+        } else {
+        scrollTo = bannerSection;
+        }
+
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(scrollTo).offset().top - 70
+        }, 1000);
+    });
+
+   
 })
+
